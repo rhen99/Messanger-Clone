f4a84e32ece822662ac9b06e1434cb3a3e36b01d
Arhen Santiago <lincoldarhen9900@gmail.com>
2021-02-02 14:59:43 +0800

    message form done

--- src/components/MessageForm/MessageForm.js
+++ src/components/MessageForm/MessageForm.js
@@ -1,8 +1,34 @@
+import "./MessageForm.scss";
+import image from '../../icons/image.svg';
+import emoji from '../../icons/emoji.svg';
+import like from '../../icons/like.svg';
+//import send from '../../icons/send.svg';
 function MessageForm() {
     return (
-        <div>
-            
-        </div>
+        <section className="message-box">
+            <nav className="message-box-nav">
+                <ul className="message-box-list">
+                    <li className="message-box-item">
+                        <a href="!#">
+                            <img src={image} alt=""/>
+                        </a>
+                    </li>
+                    <li className="message-box-item">
+                        <a href="!#">
+                            <img src={emoji} alt=""/>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+            <form action="#" className="message-box-form">
+                <div className="message-box-div">
+                    <input name="message" id="" className="message-box-input" placeholder="Write your message here..."/>
+                </div>
+                <div className="message-box-div">
+                    <button type="submit" className="message-box-submit"><img src={like} alt=""/></button>
+                </div>
+            </form>
+        </section>
     )
 }
 

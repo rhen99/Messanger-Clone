2a688f79c2a7ac85aa29985ad100b5b532cdaad3
Arhen Santiago <lincoldarhen9900@gmail.com>
2021-02-02 18:56:46 +0800

    ui done

--- src/components/MessageForm/MessageForm.js
+++ src/components/MessageForm/MessageForm.js
@@ -1,9 +1,11 @@
+import { useState } from 'react'
 import "./MessageForm.scss";
 import image from '../../icons/image.svg';
 import emoji from '../../icons/emoji.svg';
 import like from '../../icons/like.svg';
-//import send from '../../icons/send.svg';
+import send from '../../icons/send.svg';
 function MessageForm() {
+    const [msg, setMsg] = useState('');
     return (
         <section className="message-box">
             <nav className="message-box-nav">
@@ -22,10 +24,10 @@ function MessageForm() {
             </nav>
             <form action="#" className="message-box-form">
                 <div className="message-box-div">
-                    <input name="message" id="" className="message-box-input" placeholder="Write your message here..."/>
+                    <input name="message" id="" className="message-box-input" placeholder="Write your message here..." onChange={(e) => setMsg(e.target.value) } value={msg}/>
                 </div>
                 <div className="message-box-div">
-                    <button type="submit" className="message-box-submit"><img src={like} alt=""/></button>
+                    <button type="submit" className="message-box-submit"><img src={msg ? send : like} alt=""/></button>
                 </div>
             </form>
         </section>
--- src/components/Options/Options.js
+++ src/components/Options/Options.js
@@ -1,7 +1,30 @@
+import './Options.scss';
+import down from '../../icons/down.svg';
 function Options() {
     return (
-        <section>
-            
+        <section className="options">
+            <header className="options-header">
+                <div className="options-header-avatar">
+                    <div></div>
+                </div>
+                <h1 className="options-header-name">John Doe</h1>
+            </header>
+            <nav className="options-nav">
+                <ul className="options-nav-list">
+                    <li className="options-nav-item">
+                        <a href="!#">
+                            Customize Chat
+                            <img src={down} alt=""/>
+                        </a>
+                    </li>
+                    <li className="options-nav-item">
+                        <a href="!#">
+                            Privacy & Support
+                            <img src={down} alt=""/>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
         </section>
     )
 }
